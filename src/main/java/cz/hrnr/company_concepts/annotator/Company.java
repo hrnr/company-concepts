a39186f1f36622c11dbd57e61cb715465dede316
Jiri Horner <laeqten@gmail.com>
2016-06-30 18:11:11 +0200

    add toString to Company class
    * uses reflection to print fields

--- src/main/java/cz/hrnr/company_concepts/annotator/Company.java
+++ src/main/java/cz/hrnr/company_concepts/annotator/Company.java
@@ -3,6 +3,8 @@ package cz.hrnr.company_concepts.annotator;
 import java.net.URL;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
 public class Company {
 	private String name;
 	private URL url;
@@ -11,19 +13,29 @@ public class Company {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public URL getUrl() {
 		return url;
 	}
+
 	public void setUrl(URL url) {
 		this.url = url;
 	}
+
 	public List<String> getKeywords() {
 		return keywords;
 	}
+
 	public void setKeywords(List<String> keywords) {
 		this.keywords = keywords;
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
 	}
 }
