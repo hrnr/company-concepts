37ff5f8299e0d166b74fb08a40ad51fb16d0c01c
Jiri Horner <laeqten@gmail.com>
2016-07-01 16:14:07 +0200

    filter keywords from watson
    improve results with several heuristics:
    
    * do not include gompany name in keywords
    * remove invalid keywords wrongly extracted from html
    * remove duplicities

--- src/main/java/cz/hrnr/company_concepts/watson/WatsonCompanyConceptsAnalyzer.java
+++ src/main/java/cz/hrnr/company_concepts/watson/WatsonCompanyConceptsAnalyzer.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
 import com.ibm.watson.developer_cloud.alchemy.v1.model.Concept;
@@ -14,11 +17,38 @@ import com.ibm.watson.developer_cloud.alchemy.v1.model.Keywords;
 import cz.hrnr.company_concepts.annotator.Company;
 import cz.hrnr.company_concepts.annotator.CompanyConceptsAnalyzer;
 
+/**
+ * Uses IBM watson REST api to get the most relevant concepts (keywords) for
+ * company. Curently uses solely company's webpage (url) to extract keywords.
+ *
+ * @author henry
+ *
+ */
 public class WatsonCompanyConceptsAnalyzer implements CompanyConceptsAnalyzer {
 	AlchemyLanguage service;
 
 	public WatsonCompanyConceptsAnalyzer(Credentials credentials) {
 		service = new AlchemyLanguage(credentials.getApikey());
+	}
+
+	/**
+	 * Filters keywords with several heuristic approches to get the most
+	 * relevant ones.
+	 *
+	 * @param keywords
+	 *            keywords sorted by relevance
+	 * @return filtered keywords
+	 */
+	private List<String> filterKeywords(String companyName, List<String> keywords) {
+		return keywords.stream()
+				// filter away keywords too similar to company name
+				.filter(x -> StringUtils.getLevenshteinDistance(companyName, x, 2) == -1)
+				// badly recognized keywords
+				.filter(x -> !x.contains("\n")).filter(x -> !x.contains("\t"))
+				// remove duplicates
+				.distinct()
+				// limit to 20 keywords (hard limit)
+				.limit(20).collect(Collectors.toList());
 	}
 
 	@Override
@@ -42,7 +72,9 @@ public class WatsonCompanyConceptsAnalyzer implements CompanyConceptsAnalyzer {
 		for (Keyword keyword : keywords2.getKeywords()) {
 			keywords.add(keyword.getText());
 		}
-		return keywords;
+
+		// final filtering
+		return filterKeywords(company.getName(), keywords);
 	}
 
 }
